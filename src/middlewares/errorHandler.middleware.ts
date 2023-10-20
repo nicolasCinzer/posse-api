9f938c6a90cde8357175a4778837e90227b85346
nicolasCinzer <nicolas.cinzer00@gmail.com>
2023-10-20 17:15:23 -0300

    error handler

--- src/middlewares/errorHandler.middleware.ts
+++ src/middlewares/errorHandler.middleware.ts
@@ -1,2 +1,7 @@
-export const errorHandler = (err, req, res, next) => {}
-
+export const errorHandler = (err, req, res, next) => {
+  if (err.statusCode) {
+    res.status(err.statusCode).json({ error: err.message })
+  } else {
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+}
